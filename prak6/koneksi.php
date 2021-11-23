3314cd55243ba57d981129bca8bf806c0257b01b
bekti1800018121 <50955170+bekti1800018121@users.noreply.github.com>
2021-11-23 21:43:18 +0700

    Add files via upload

---
+++ prak6/koneksi.php
@@ -0,0 +1,12 @@
+<?php  
+$host = "localhost";
+$username = "root";
+$pw = "";
+$DBname = "akademik";
+
+$con = mysqli_connect($host, $username, $pw, $DBname);
+
+if (!$con) {
+	echo "EROR : " . mysqli_connect_error();
+	exit();
+}
